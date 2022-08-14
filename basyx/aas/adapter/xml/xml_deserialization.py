a6e8cfaf192b65f28c00dbe73a61c02d26691379
Leon Möller <leon.moeller@plt.rwth-aachen.de>
2022-08-14 20:06:25 +0200

    model: rename DataTypeDef to DataTypeDefXsd
    In V3.0RC02 DataTypeDef has been split into DataTypeDefRdf and
    DataTypeDefXsd. Since DataTypeDefRdf only consists of rdf::langString
    and currently isn't used anywhere in the DotAAS specification, just
    rename DataTypeDef to DataTypeDefXsd.

--- basyx/aas/adapter/xml/xml_deserialization.py
+++ basyx/aas/adapter/xml/xml_deserialization.py
@@ -992,7 +992,7 @@ class AASFromXmlDecoder:
         return submodel
 
     @classmethod
-    def construct_value_reference_pair(cls, element: etree.Element, value_format: Optional[model.DataTypeDef] = None,
+    def construct_value_reference_pair(cls, element: etree.Element, value_format: Optional[model.DataTypeDefXsd] = None,
                                        object_class=model.ValueReferencePair, **_kwargs: Any) \
             -> model.ValueReferencePair:
         if value_format is None:
@@ -1004,7 +1004,7 @@ class AASFromXmlDecoder:
         )
 
     @classmethod
-    def construct_value_list(cls, element: etree.Element, value_format: Optional[model.DataTypeDef] = None,
+    def construct_value_list(cls, element: etree.Element, value_format: Optional[model.DataTypeDefXsd] = None,
                              **_kwargs: Any) -> model.ValueList:
         """
         This function doesn't support the object_class parameter, because ValueList is just a generic type alias.
