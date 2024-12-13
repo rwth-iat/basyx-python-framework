a08971f815e95a6ed1d1885516001d694f2ac903
s-heppner <mail@s-heppner.com>
2024-12-13 10:11:34 +0100

    Update sdk/basyx/__init__.py

--- sdk/basyx/__init__.py
+++ sdk/basyx/__init__.py
@@ -1,4 +1,5 @@
 from .object_store import *
 
-# Shortcut to import aas_core3 which should be installed after installing requirements
+# Alias `types` as `model` to maintain consistency with the import style of the predecessor project (BaSyx-Python SDK),
+# ensuring a familiar experience for users transitioning to the new BaSyx-Python Framework.
 from aas_core3 import types as model
